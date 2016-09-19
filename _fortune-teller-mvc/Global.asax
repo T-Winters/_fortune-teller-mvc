2c33e2eeed8b12a8ed74a10ccd3e7bfa222a30ee
T-Winters <twint022@gmial.com>
2016-09-18 21:25:03 -0400

    Add project files.

---
+++ _fortune-teller-mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_fortune_teller_mvc.MvcApplication" Language="C#" %>
---
+++ _fortune-teller-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_fortune_teller_mvc.Startup))]
+namespace _fortune_teller_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
